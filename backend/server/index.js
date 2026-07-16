066d80223d714841da13b5eeffd9fa3937e5f1b8
Prajyot Sancheti <prajyotsancheti1772gmail.com>
2026-07-16 20:30:55 +0530

    changes

--- backend/server/index.js
+++ backend/server/index.js
@@ -23,8 +23,8 @@ const io = new Server(server, {
 app.set('trust proxy', true);
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 
 app.use("/api", router);
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
